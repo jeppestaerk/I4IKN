c1eb1cc6407976d16ab2fd0818f29ae1ec99a63f
root <jeppe@staerk.io>
2016-09-28 15:20:52 +0200

    file_server Færdig

--- Exercise08/file_server/file_server.cs
+++ Exercise08/file_server/file_server.cs
@@ -27,7 +27,44 @@ namespace tcp
  		/// </summary>
 		private file_server ()
 		{
-			// TO DO Your own code
+			// Opretter en socket
+			Console.WriteLine ("- Opretter en socket");
+			IPAddress serverIpAdress = IPAddress.Parse("10.0.0.1");
+			IPEndPoint serverIpEndPoint = new IPEndPoint(serverIpAdress, PORT);
+			TcpListener serverSocket = new TcpListener(serverIpEndPoint);
+			TcpClient clientSocket = default(TcpClient);
+			serverSocket.Start();
+
+			// Venter på en connect fra en klient
+			Console.WriteLine ("- Venter på en connect fra en klient");
+			clientSocket = serverSocket.AcceptTcpClient();
+			NetworkStream networkStream = clientSocket.GetStream();
+
+			// Modtager filnavn
+			Console.WriteLine ("- Modtager filnavn");
+			string requestedFile = LIB.readTextTCP(networkStream);
+
+			// Finder filstørrelsen
+			Console.WriteLine ("- Finder filstørrelsen");
+			long fileSize = LIB.check_File_Exists(requestedFile);
+
+			// Kalder metoden sendFile
+			Console.WriteLine ("- Kalder metoden sendFile");
+			if(fileSize > 0)
+			{
+				Console.WriteLine ("- Sender fil");
+				LIB.writeTextTCP(networkStream, "Sending file");
+				sendFile (requestedFile, fileSize, networkStream);
+			}
+			else
+			{
+				LIB.writeTextTCP (networkStream, "Error sending file");
+			}
+
+			// Lukker socketen og programmet
+			Console.WriteLine ("- Lukker socketen og programmet");
+			serverSocket.Stop();
+			clientSocket.Close();
 		}
 
 		/// <summary>
@@ -44,7 +81,38 @@ namespace tcp
 		/// </param>
 		private void sendFile (String fileName, long fileSize, NetworkStream io)
 		{
-			// TO DO Your own code
+			// Sends the file
+			FileStream fileStream;
+			int numberOfPackets = 0;
+			int totalLenght = 0;
+
+			fileStream = new FileStream (fileName, FileMode.Open, FileAccess.Read);
+			numberOfPackets = Convert.ToInt32(Math.Ceiling (Convert.ToDouble(fileStream.Length) / Convert.ToDouble(BUFSIZE)));
+			totalLenght = Convert.ToInt32(fileStream.Length);
+
+			LIB.writeTextTCP(io, fileSize.ToString());
+
+			for(int i = 0; i < numberOfPackets; i++)
+			{
+				int currentPacketsLenght = 0;
+
+				if(totalLenght > BUFSIZE)
+				{
+					currentPacketsLenght = BUFSIZE;
+					totalLenght -= currentPacketsLenght;
+				}
+				else
+				{
+					currentPacketsLenght = totalLenght;
+				}
+
+				byte[] sendingBuffer = new byte[currentPacketsLenght];
+				fileStream.Read(sendingBuffer, 0, currentPacketsLenght);
+				io.Write(sendingBuffer, 0, currentPacketsLenght);
+			}
+
+			fileStream.Close();
+			io.Close();
 		}
 
 		/// <summary>
